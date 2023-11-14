3025adfe7b82aa84ad7c3e54396a00783f516780
ahmad <dardaumusaahmad111@gmail.com>
2023-11-14 17:14:37 +0000

    modified

--- src/components/one.jsx
+++ src/components/one.jsx
@@ -1,14 +1,32 @@
 
 
 import  "../styles/One.css"
-import "../assets/images/Group/ 688.png"
+import "../assets/images/Rectangle26.png"
+import plane from "../assets/images/plane.png"
 
 const One = () => {
     return ( 
         <>
         <main>
             <aside className="left"></aside>
-            <aside className="right"></aside>
+            <aside className="right">
+                <div className="plane">
+                    <img src={plane} alt="" />
+                </div>
+                <div className="welcome">
+                    <h1>Welcome</h1>
+                    <p>Login with Email</p>
+                </div>
+                <div className="form">
+                    <fieldset className="email">
+                        <legend>Email id</legend>
+                    </fieldset>
+                    <fieldset className="password">
+                        <legend>Password</legend>
+                    </fieldset>
+                    <p>Forgot your password</p>
+                </div>
+            </aside>
         </main>
             
         </>
